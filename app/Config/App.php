747c784762e4509a58c839c1b98923652b654dc9
Emerson <emersonsilvo@gmail.com>
2025-05-07 14:10:22 -0300

    set index

--- app/Config/App.php
+++ app/Config/App.php
@@ -26,7 +26,7 @@ class App extends BaseConfig
 	
 	//public $baseURL = "https://www.labpix.com.br/clientes/lemme/";
 	//public $baseURL = "http://localhost/manha/";
-	public $baseURL = "https://aprovacao-ccd5a7cgbrguczdk.canadacentral-01.azurewebsites.net/manha/";
+	public $baseURL = "https://aprovacao-ccd5a7cgbrguczdk.canadacentral-01.azurewebsites.net/";
 
 	
 
